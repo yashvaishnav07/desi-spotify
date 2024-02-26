fe6de753f1c01ffb0d7653f59be96a31e5bfdbd4
yashvaishnav07 <megaparadox7@gmail.com>
2024-02-26 12:11:56 +0530

    New

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,7 +31,7 @@ export default function Login() {
   const navigate = useNavigate();
 
   const getAuthorized = async () => {
-    const clientId = process.env.REACT_APP_CLIENT_ID_PRE
+    const clientId = 'f568c23d98424cb58c341c2660524dc7'
     const scope = ['user-read-playback-state', 'user-read-playback-state', 'user-modify-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'user-read-playback-position', 'user-top-read', 'user-read-recently-played', 'user-read-email', 'user-read-private']
     const redirectUrl = "http://localhost:3000/desi-spotify"
     const apiUrl = "https://accounts.spotify.com/authorize"
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,8 @@
 import fetchAccessToken from "./fetchAccessToken";
 
-export default async function fetchData(url) {
-  const clientId = process.env.REACT_APP_CLIENT_ID_PRE;
-  const clientSecret = process.env.REACT_APP_CLIENT_SECRET_PRE;
+export default async function fetchData(url) { 
+  const clientId = 'f568c23d98424cb58c341c2660524dc7';
+  const clientSecret = '3782ec241b3641248ffca3e202687bb8';
   const accessToken = await fetchAccessToken(clientId, clientSecret);
   const res = await fetch(url, {
     method: "GET",
